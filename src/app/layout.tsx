0b2e0d7acf3be6f06979fb6dd0934c89b84f7208
Angel Moura <angelmurahs829@gmail.com>
2025-08-25 01:03:17 +0000

    NAO, O GTM TÁ CERTO, O GTM NÃO TEM PIXEL DO FACEBOOK PORRA, O PIXEL DO F

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,27 +23,8 @@ export default function RootLayout({
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PG63VDLP');
-`,
-          }}
-        />
       </head>
       <body className="font-body antialiased">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PG63VDLP"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <Suspense fallback={null}>
           <N8NTracker />
         </Suspense>
